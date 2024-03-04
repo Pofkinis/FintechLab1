94c8b2be2445c39165c239bae38eda229b048ce5
Povilas Bačkierius <povilas.backierius@surfsharkteam.com>
2024-03-04 21:19:01 +0200

    Add button functionality to sau hello world

--- app/src/main/java/com/example/fintechlab1/MainActivity.java
+++ app/src/main/java/com/example/fintechlab1/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.fintechlab1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = findViewById(R.id.textView2);
+    }
+
+    public void OnBtnclick(View view) {
+        this.textView.setText("Hello world!");
     }
 }

ce936a979893ad081de81e0cec647bd4df5ef17b
Povilas Bačkierius <povilas.backierius@surfsharkteam.com>
2024-03-04 20:38:55 +0200

    Initial commint

---
+++ app/src/main/java/com/example/fintechlab1/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.fintechlab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FintechLab1"
+include(":app")
+ 
